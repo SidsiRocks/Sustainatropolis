9924286994b8a5772d3cd973468d459326658679
Siddhartha Singh <sidsiskilled@gmail.com>
2024-04-16 21:39:41 +0530

    Added part of the settings UI

---
+++ game/hideOnCloseWindow.py
@@ -0,0 +1,23 @@
+import pygame
+
+from pygame_gui.core import ObjectID
+from typing import Union, Tuple, Optional
+from pygame_gui.elements.ui_window import UIWindow
+from pygame_gui.core.interfaces import IUIManagerInterface
+
+class HideUIwindow(UIWindow):
+    def __init__(self,
+                 rect: pygame.Rect,
+                 manager: Optional[IUIManagerInterface] = None,
+                 window_display_title: str = "",
+                 element_id: Optional[str] = None,
+                 object_id: Optional[Union[ObjectID, str]] = None,
+                 resizable: bool = False,
+                 visible: int = 1,
+                 draggable: bool = True):
+        super().__init__(rect,manager,
+                window_display_title,element_id,
+                object_id,resizable,visible,
+                draggable)
+    def on_close_window_button_pressed(self):
+        self.hide()
---
+++ game/settingsUI.py
@@ -0,0 +1,58 @@
+from pygame_gui.elements.ui_window import UIWindow
+from pygame_gui.elements.ui_horizontal_slider import UIHorizontalSlider
+from pygame_gui.elements.ui_button import UIButton
+from pygame_gui.elements.ui_label import UILabel
+from pygame_gui.core import ObjectID
+from pygame import Rect
+from .hideOnCloseWindow import HideUIwindow
+import pygame_gui
+
+class SettingsUI:
+    def __init__(self,manager):
+        self.settingsWindow,self.horizontalSlider,self.settingsButton = self.createSettingsWindow(manager)
+    def createSettingsWindow(self,manager):
+        settingsWinWidth = 380
+        settingsWinHeight = 500
+
+        windowWidth = manager.window_resolution[0]
+        windowHeight = manager.window_resolution[1]
+
+        settButtonWidth  = 80
+        settButtonHeight = 80
+        settingsRect = Rect(windowWidth-settButtonWidth,0,settButtonWidth,settButtonHeight)
+        settingsButton = UIButton(relative_rect=settingsRect,
+                                  text="Settings",manager=manager,
+                                  object_id=ObjectID("#settingsButton","@settingsButton"))
+
+        settingsWinRect = Rect((windowWidth-settingsWinWidth)//2,(windowHeight-settingsWinHeight)//2,
+                               settingsWinWidth,settingsWinHeight)
+        settingsWindow = HideUIwindow(rect=settingsWinRect,manager=manager,
+            window_display_title="Settings Window",
+            object_id=ObjectID("#settingsWin","@settingsWin"),
+            resizable=False,draggable=False,visible=0)
+        settingsWindow.set_blocking(True)
+        settingsWindow.on_close_window_button_pressed
+        padY = 30
+        sliderWidth = 200
+        labelWidth = 100
+        sliderHeight = 30
+        leftPos = (settingsWinWidth-sliderWidth-labelWidth)//2
+        
+        labelRect = Rect(leftPos,padY,labelWidth,sliderHeight)
+        sliderRect = Rect(leftPos+labelWidth,padY,sliderWidth,sliderHeight)
+        horizontalSlider = UIHorizontalSlider(relative_rect=sliderRect,
+                            start_value=100,value_range=(0,100),
+                            manager=manager,container=settingsWindow,
+                            object_id=ObjectID("#horizontalSlider","@horizontalSlider"))
+        settingsLabel = UILabel(relative_rect=labelRect,text="Music Volume",manager=manager,
+                                container=settingsWindow,
+                                object_id=ObjectID("#settingsLabel","@settingsLabel"))
+        return settingsWindow,horizontalSlider,settingsButton
+
+    def update(self):
+        sliderVal = self.horizontalSlider.get_current_value()
+    def processEvent(self,event):
+        if event.type == pygame_gui.UI_BUTTON_PRESSED:
+            objectID = event.ui_object_id
+            if objectID == "#settingsButton":
+                self.settingsWindow.show()
