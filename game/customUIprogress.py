9c71a6e1d15b6c7d630317ec9184b1f81cb8f109
Siddhartha Singh <sidsiskilled@gmail.com>
2024-04-19 12:33:51 +0530

    Fixed customUIprogress bar

--- game/customUIprogress.py
+++ game/customUIprogress.py
@@ -36,6 +36,20 @@ class CustomUIprogressBar(UIStatusBar):
             return min(self.current_progress/ self.maximum_progress,1)
         else:
             return 0
+        
+    @property 
+    def percent_full(self):
+        if self.maximum_progress != 0:
+            return min(self.current_progress/ self.maximum_progress,1)
+        else:
+            return 0
+
+    @percent_full.setter 
+    def percent_full(self,value):
+        value = min(value,1)
+        if value != self.percent_full:
+            self._percent_full = value
+            self.status_changed = True
     def status_text(self):
 #        print("current progress is:",self.current_progress,"maximum is:",self.maximum_progress)
         return f"{self.current_progress:0.1f}/{self.maximum_progress:0.1f}"
